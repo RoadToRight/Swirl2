2cd175d13a47f257f120a02d9cb2de7a83f460bf
Sameer <syedsameershah2008@gmail.com>
2025-01-26 10:29:37 -0800

    all done

--- SwirlFrontend/src/Components/Loader2.jsx
+++ SwirlFrontend/src/Components/Loader2.jsx
@@ -5,7 +5,7 @@ const Loader2 = () => {
 <div className='bg-blue'>
 <div className='h-screen bg-[#000000] text-white font-bold text-[30px] z-[2000] fixed w-screen flex justify-center items-center top-0'>
    <Helmet>
-        <link rel="preload" href="/Project IMG/logoanime.mp4" as="video" type="video/mp4" />
+        <link rel="preload" href="/Project IMG/logoanime.webm" as="video" type="video/webm" />
       </Helmet>
       <video autoPlay muted loop  preload="auto">
       <source src="/Project IMG/logoanime.webm" type="video/webm" />
